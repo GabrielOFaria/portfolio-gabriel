6d1e76b1a846d015ef6fb6ec9de48cd508d60430
Gabriel <gabrielfariaw@gmail.com>
2023-11-19 13:18:41 -0300

    Start portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { styles } from "../styles";
+import { navLinks } from "../constants/index";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState("");
+  return;
+  <div>
+    <nav className="`{${styles.paddingX} }`">
+
+    </nav>
+  </div>;
+};
+
+export default Navbar;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+const styles = {
+    paddingX: 'sm:px-16 px-6',
+    paddingY: 'sm:px-16 px-6',
+    padding: 'sm:px-16 px-6 sm:py16 py-10',
+
+    heroHeadText:
+     'font-black text-while lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2'
+    
+};
+
+
+export {styles};
